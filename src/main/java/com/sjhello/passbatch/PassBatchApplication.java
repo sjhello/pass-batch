cb549356aafe03902b368bfe0de6ecb4ebe97b09
sjhello <sjhello235@gmail.com>
2023-05-10 22:51:52 +0900

    sample

--- src/main/java/com/sjhello/passbatch/PassBatchApplication.java
+++ src/main/java/com/sjhello/passbatch/PassBatchApplication.java
@@ -1,10 +1,47 @@
 package com.sjhello.passbatch;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.log4j.Log4j2;
+
+@EnableBatchProcessing
 @SpringBootApplication
+@RequiredArgsConstructor
+@Log4j2
 public class PassBatchApplication {
+
+	private final JobBuilderFactory jobBuilderFactory;
+
+	private final StepBuilderFactory stepBuilderFactory;
+
+	@Bean
+	public Step passStep() {
+		return this.stepBuilderFactory.get("passStep")
+			.tasklet((contribution, chunkContext) -> {
+				log.info("passStep");
+				return RepeatStatus.FINISHED;
+			}).build();
+	}
+
+	@Bean
+	public Job passJob() {
+		return this.jobBuilderFactory.get("passJob")
+			.start(passStep())
+			.build();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PassBatchApplication.class, args);
